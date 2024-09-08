e5025cee9c9747f457fc0c5860396cca71211e66
Pedro Pereira <PedroJoaoPereira@users.noreply.github.com>
2024-09-08 19:38:52 +0100

    refactor: changes post install customization script for gentoo-personal-laptop

---
+++ deprecated/notes.sh
@@ -0,0 +1,4 @@
+### WIP
+# emerge sys-process/cronie
+# rc-update add sshd default
+# rc-update add cronie default
--- hosts/gentoo_personal_laptop/post_install_gentoo.sh
+++ hosts/gentoo_personal_laptop/post_install_gentoo.sh
@@ -11,27 +11,35 @@ echo 'app-editors/vscode Microsoft-vscode' >>/etc/portage/package.license
 echo 'net-im/discord all-rights-reserved' >>/etc/portage/package.license
 echo 'sys-firmware/intel-microcode intel-ucode' >>/etc/portage/package.license
 echo 'www-client/google-chrome google-chrome' >>/etc/portage/package.license
-# installs dependencies
 emerge --ask=n \
   app-editors/vscode \
   gnome-base/gnome-light \
   net-im/discord \
   sys-firmware/intel-microcode \
+  sys-power/power-profiles-daemon \
   www-client/google-chrome
 if [ $? -ne 0 ]; then
   echo 'Error installing extra dependencies'
   exit 1
 fi
-# reads news items
 eselect news read >/dev/null 2>&1
-# reloads environment variables
-env-update && source /etc/profile
-# sets up openrc services
-rc-update add elogind boot
 
-### WIP
-emerge sys-process/cronie
+echo -e '\n### Reloading environment...\n'
+env-update && source /etc/profile
+
+echo -e '\n### Setting up customizations...\n'
+# overrides wheel use polkit rules
+mkdir -p /etc/polkit-1/rules.d/
+cat <<EOF >/etc/polkit-1/rules.d/49-wheel.rules
+polkit.addAdminRule(function(action, subject) {
+    return ["unix-group:wheel"];
+});
+EOF
+# sets display manager to gdm
+sed -i "s/.*DISPLAYMANAGER=.*/DISPLAYMANAGER=\"gdm\"/g" /etc/conf.d/display-manager
+# sets up openrc services
 rc-update add NetworkManager default
+rc-update add display-manager default
+rc-update add elogind boot
 rc-update add numlock default
-rc-update add sshd default
-rc-update add cronie default
+rc-update add power-profiles-daemon default
